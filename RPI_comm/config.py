f93c1f13a8e89e62008de25b3ad09b946e007c96
sherrysheng0208 <35883711+sherrysheng0208@users.noreply.github.com>
2021-02-19 14:46:23 +0800

    Add files via upload

---
+++ RPI_comm/config.py
@@ -0,0 +1,13 @@
+#Device Configuration Script
+
+#PC - WIFI Settings
+wifi_ip_address = '192.168.5.5'
+wifi_port = 5555
+wifi_port_ir = 5000
+
+#Android Settings
+unique_id ="0000110A-0000-1000-8000-00805F9B34FB"
+
+#Arduino Settings
+ser_port = '/dev/ttyACM0'
+baudrate = 115200
